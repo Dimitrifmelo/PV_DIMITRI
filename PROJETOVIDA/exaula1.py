d2dcf8b6c806a4f79b7a9025a8ba87fdb5d46c90
Dimitrifmelo <126780940+Dimitrifmelo@users.noreply.github.com>
2023-10-20 11:05:39 -0300

    Add files via upload

---
+++ PROJETOVIDA/exaula1.py
@@ -0,0 +1,18 @@
+
+#exercicio 1
+print("O primeiro programa a gente nunca esquece!")
+
+#exercicio 2
+print("Dimitri Melo")
+print("Bauru")
+print("dimizilla@gmail.com")
+
+#exercicio
+print("XXXXX")
+print("X   X")
+print("X   X")
+print("X   X")
+print("XXXXX")
+
+
+
